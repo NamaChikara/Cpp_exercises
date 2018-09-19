053dcec391c4abe612c2941f98d4362d6cd2c4e4
Zachary <zackbarry13@gmail.com>
2018-09-19 15:40:47 -0700

    Added a class, Nodeselektor, which has Binary_tree as its base and overloads the virtual draw_nodes() function to turn the nodes into rectangles.

--- Binary_tree_FLTK.cpp
+++ Binary_tree_FLTK.cpp
@@ -1,9 +1,10 @@
 // Binary_tree is a class that, given the number of rows and desired spacing, produces a binary tree.
 
-// recommended headers for the graphics chapters in "Programming Practices and Principles in C++"
-//  the graphics package FLTK is required for compilation, \
-//  the other headers can be found at http://stroustrup.com/Programming/
+//  NOTE: due to the complex nature of the required headers and the FLTK compiler/linker settings,
+//	 attempts to split this code up into a separate header and cpp file for the classes have failed.
+//	 I have elected to keep this somewhat cluttered set up in interest of time.
 
+// recommended headers for the graphics chapters in "Programming Practices and Principles in C++"
 #include "C:/Users/zackb_000/Documents/Visual Studio 2017/Projects/ProgrammingPrincPracC/fltk.h"
 #include "C:/Users/zackb_000/Documents/Visual Studio 2017/Projects/ProgrammingPrincPracC/Simple_window.h"
 #include "C:/Users/zackb_000/Documents/Visual Studio 2017/Projects/ProgrammingPrincPracC/Graph.h"
@@ -28,27 +29,44 @@ void print_point(Point p) {
 //  h_space is the spacing between nodes in the final row, v_space is spacing between rows
 class Binary_tree : public Shape {
 public:
-	Binary_tree(Point start, int rows, int h_space, int v_space1)
-		: start{ start }, rows{ rows }, h_space{ h_space }, v_space{ v_space1 }
-	{
-		if (rows > 0) {
-			generate_centers();
-			draw_lines();
-		}
-		else
-			std::cout << "Empty binary tree." << std::endl;
-	}
-
-private:
+	Binary_tree(Point start, int rows, int h_space, int v_space);
+	Binary_tree(Point start, int rows, int h_space, int v_space, int radius);
+protected:
 	Point start;				// location of first node
 	int rows;					// layers of the tree
 	int h_space;				// distance between nodes within the last row
 	int v_space;				// distance between rows of nodes
+	int radius = 5;				// for the node circles
 	void generate_centers();	// store the center of the nodes in centers
-	void draw_lines() const;	// override Shape's virtual function
+	void draw_lines() const;	// override Shape's virtual function	
+	virtual void draw_nodes() const;
+	void draw_connections() const;
 	vector<vector<Point>> centers;		// store the center of the nodes
 };
-
+// in case the user provides a radius for the node:
+Binary_tree::Binary_tree(Point start, int rows, int h_space, int v_space, int radius)
+	: start{ start }, rows{ rows }, h_space{ h_space }, v_space{ v_space }, radius{ radius }
+{
+	if (rows > 0) {
+		generate_centers();
+		draw_lines();
+	}
+	else
+		std::cout << "Empty binary tree." << std::endl;
+}
+// in case the user doesn't provide a radius for the node:
+Binary_tree::Binary_tree(Point start, int rows, int h_space, int v_space)
+	: start{ start }, rows{ rows }, h_space{ h_space }, v_space{ v_space }
+{
+	if (rows > 0) {
+		generate_centers();
+		draw_lines();
+	}
+	else
+		std::cout << "Empty binary tree." << std::endl;
+}
+// loads a vector of vectors - each interior vector represents a row, 
+//   and contains the centers of the nodes for that row
 void Binary_tree::generate_centers() {
 	vector<Point> row;
 	row.push_back(start);	// the first row is the starting point
@@ -65,15 +83,23 @@ void Binary_tree::generate_centers() {
 		row.clear();
 	}
 }
-
+// replaces virtual function from Shape class
 void Binary_tree::draw_lines() const {
-	// first, draw the circle
+	// first, draw the circles
+	draw_nodes();
+	// then connect the nodes
+	draw_connections();
+}
+// draws circles in the locations specified by centers
+void Binary_tree::draw_nodes() const {
 	for (int i = 0; i < centers.size(); ++i) {
 		for (int j = 0; j < centers[i].size(); ++j) {
-			Graph_lib::Circle{ centers[i][j],5 }.draw_lines();
+			Graph_lib::Circle{ centers[i][j], radius }.draw_lines();
 		}
 	}
-	// next, draw the connecting arcs
+}
+// draws arcs connecing nodes in the form of partial ellipses
+void Binary_tree::draw_connections() const {
 	int row_space;
 	for (int i = 0; i < rows - 1; ++i) {
 		row_space = h_space * std::pow(2, rows - i - 1);
@@ -84,18 +110,47 @@ void Binary_tree::draw_lines() const {
 	}
 }
 
+// uses rectangles for nodes instead of circles
+class Nodeselektor : public Binary_tree {
+public:
+	Nodeselektor(Point start, int rows, int h_space, int v_space, int ww, int hh);
+	Nodeselektor(Point start, int rows, int h_space, int v_space);
+	void draw_nodes() const;
+	int width = 10;
+	int height = 10;
+};
+// if width(ww) and height(hh) are provided by the user:
+Nodeselektor::Nodeselektor(Point start, int rows, int h_space, int v_space, int ww, int hh)
+	: Binary_tree(start, rows, h_space, v_space), width{ ww }, height{ hh }
+{
+	draw_lines();
+}
+// if width and height aren't provided by the user:
+Nodeselektor::Nodeselektor(Point start, int rows, int h_space, int v_space)
+	: Binary_tree(start, rows, h_space, v_space)
+{
+	draw_lines();
+}
+
+// nodes are boxes instead of circles (as in Binary_tree)
+void Nodeselektor::draw_nodes() const {
+	for (int i = 0; i < centers.size(); ++i) {
+		for (int j = 0; j < centers[i].size(); ++j) {
+			// the point that determines a rectangle is the top left corner, not the center
+			Point tl(centers[i][j].x - width / 2, centers[i][j].y - height / 2);
+			Graph_lib::Rectangle{ tl, width, height }.draw_lines();
+		}
+	}
+}
+
 int main() {
 	Simple_window win{ {0,0},1600,800,"Binary Tree" };
 	Binary_tree bin_tree{ {800,10},5,40,70 };
 	bin_tree.set_color(Color::black);
 	win.attach(bin_tree);
 	win.wait_for_button();
-	Binary_tree bin_tree2{ {800,25}, 1, 40, 80 };
+	Nodeselektor bin_tree2{ {800,25}, 3, 40, 80 };
 	win.attach(bin_tree2);
 	bin_tree2.set_color(Color::red);
-	win.wait_for_button();
-	Binary_tree bin_tree3{ {800,35}, 0, 40, 80 };
-	win.attach(bin_tree3);
-	bin_tree3.set_color(Color::blue);
 	win.wait_for_button();
 }
