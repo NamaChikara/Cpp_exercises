e1a94de6ef1512904c13898aaeb1b7bda63003e4
Zachary <zackbarry13@gmail.com>
2018-09-21 16:32:48 -0700

    Updated with facilities needed for Currency_window in Currency_Converter.cpp

--- Currency_converter/Currency.cpp
+++ Currency_converter/Currency.cpp
@@ -1,7 +1,10 @@
+// Defines operations for (country, currency) pairs (i.e. the struct Conversion).
+
 #include "Currency.h"
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
 
 Conversion::Conversion(std::string s, double d)
 	: country{ s }, usd_ratio{ d } {}
@@ -9,7 +12,6 @@ Conversion::Conversion(std::string s, double d)
 Conversion::Conversion()
 	: country{ "" }, usd_ratio{ 0 } {}
 
-// used to verify read_rates is working correctly
 void print_conversion(const std::vector<Conversion>& rates) {
 	for (Conversion c : rates)
 		std::cout << c.country << '\t' << c.usd_ratio << std::endl;
@@ -26,12 +28,65 @@ std::istream& operator>> (std::istream& ist, Conversion& rate)
 }
 
 // provide an input file stream and use overloaded >> to read Conversions
-std::vector<Conversion> read_rates(std::string file) {
+std::vector<Conversion> read_ratios(std::string file) {
 	std::ifstream ifs{ file };
-	if (!ifs) throw "can't open " + file;
+	if (!ifs) throw my_error("can't open " + file);
 	Conversion c;
 	std::vector<Conversion> rates;
-	while (ifs >> c)
+	while (ifs >> c)		// read Conversions into rates
 		rates.push_back(c);
 	return rates;
+}
+
+// convert money (n) from initial currency to final currency
+double convert_money(double to_convert, std::string initial_c, std::string final_c,
+	std::vector<Conversion> ratios)
+{
+	double initial_ratio = 0;
+	double final_ratio = 0;
+	// iterate over vector of ratios to find appropriate values
+	for (std::vector<int>::size_type i = 0; i != ratios.size(); ++i)
+	{
+		if (ratios[i].country == initial_c)
+			initial_ratio = ratios[i].usd_ratio;
+		if (ratios[i].country == final_c)
+			final_ratio = ratios[i].usd_ratio;
+	}
+	if (initial_ratio == 0 && final_ratio == 0)
+		throw my_error("Currency types " + initial_c + '\t' + final_c + " not found.");
+	double converted = (double)to_convert * (initial_ratio / final_ratio);
+	return converted;
+}
+
+// return the country names from a vector of Conversions
+std::string get_countries(std::vector<Conversion> cons)
+{
+	std::string names;
+	// build the string of names by iterating through cons
+	for (std::vector<int>::size_type i = 0; i < cons.size(); ++i) {
+		if (i != cons.size() - 1)
+			names += cons[i].country + ", ";
+		else
+			names += cons[i].country + ".";
+	}
+	return names;
+}
+
+// convert from string to double
+double str_to_double(std::string s)
+{
+	std::istringstream is{ s };
+	double d;
+	is >> d;
+	if (!is) my_error("double format error: " + s);
+	return d;
+}
+
+// convert from double to string
+std::string double_to_str(double d)
+{
+	std::ostringstream os;
+	os << d;
+	std::string s = os.str().c_str();
+	return s;
 }
--- Currency_converter/Currency.h
+++ Currency_converter/Currency.h
@@ -1,3 +1,5 @@
+// Creates storage and operations for (country, currency) pairs.
+
 #pragma once
 
 #ifndef Currency_H
@@ -8,18 +10,39 @@
 
 // Stores country name and currency value relative to USD
 struct Conversion {
-	Conversion(std::string s, double d);
+	Conversion(std::string, double);
 	Conversion();
 	std::string country;
 	double usd_ratio;
 };
 
-void print_conversion(const std::vector<Conversion>& rates);
+void print_conversion(const std::vector<Conversion>&);
 
 // read into a Conversion
-std::istream& operator>> (std::istream& ist, Conversion& rate);
+std::istream& operator>> (std::istream&, Conversion&);
 
 // provide an input file stream and use overloaded >> to read Conversions
-std::vector<Conversion> read_rates(std::string file);
+std::vector<Conversion> read_ratios(std::string);
+
+// convert money from initial currency to final currency using conversions
+double convert_money(double, std::string, std::string, std::vector<Conversion>);
+
+// get the country names from a vector of Conversions
+std::string get_countries(std::vector<Conversion>);
+
+// convert from string to double
+double str_to_double(std::string);
+
+// convert from double to string
+std::string double_to_str(double);
+
+// error handling
+struct my_error : public std::exception
+{
+	std::string s;
+	my_error(std::string ss) : s{ ss } {}
+	~my_error() throw() {}
+	const char* what() const throw() { return s.c_str(); }
+};
 
 #endif 
